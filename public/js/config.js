1e4336c72a95f9eebf37f6ae920dbd30d04be393
Praneetha <praneetha1506@gmail.com>
2025-12-19 21:44:01 +0530

    Set production config + index redirect + theme

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 window.ADMIN_CONFIG = {
-  API_BASE_URL: '',
-  FRONTEND_URL: ''
+  API_BASE_URL: 'https://portfolio-backend-7q1v.onrender.com',
+  FRONTEND_URL: 'https://portfolio-frontend-omega-rosy.vercel.app'
 };
